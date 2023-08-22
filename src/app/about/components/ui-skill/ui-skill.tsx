8dde3ae060241f748bdefb037a18bb15a9ec8fcf
jeyson <jeysonkmguzman@gmail.com>
2023-08-22 13:07:29 -0500

    feat: finish about page

--- src/app/about/components/ui-skill/ui-skill.tsx
+++ src/app/about/components/ui-skill/ui-skill.tsx
@@ -1,16 +1,23 @@
+'use client';
 import { LIST_SKILL } from '@/constants';
 import { GridLayout } from '@/layout';
 import { UiIcon } from '@/components';
+import style from './ui-skill.module.scss';
+import { useTranslate } from '@/hooks';
 
 const size = 80;
 
 function UiSkill(): JSX.Element {
+  const { onTranslate } = useTranslate();
   return (
-    <GridLayout sizeMin={size}>
-      {LIST_SKILL.map((item) => (
-        <UiIcon icon={item} size={size} code={true} key={item} title={item}/>
-      ))}
-    </GridLayout>
+    <div>
+      <h3 className={style.titleSkill}>{onTranslate('technology')}</h3>
+      <GridLayout sizeMin={size}>
+        {LIST_SKILL.map((item) => (
+          <UiIcon icon={item} size={size} code={true} key={item} title={item} />
+        ))}
+      </GridLayout>
+    </div>
   );
 }
 
