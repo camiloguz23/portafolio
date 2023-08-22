8fbddae6bfe922d19e6caa5d06f99165f59fc0c9
jeyson <jeysonkmguzman@gmail.com>
2023-08-22 16:44:20 -0500

    fix: Iconcomponents

--- src/components/ui-icon/ui-icon.tsx
+++ src/components/ui-icon/ui-icon.tsx
@@ -10,10 +10,13 @@ interface Prop {
   code?: boolean;
   clear?: boolean;
 }
+
+type IconComponent = (props: { size?: number; color?: string }) => JSX.Element;
+
 function UiIcon({ icon, color = 'rgba(123, 74, 226, 0.50)', size = 24, code = false, title = '', clear = false }: Prop): JSX.Element {
   const getIcon = (name: string, size: number, color: string, code: boolean): React.ReactElement => {
     const nameConstructor = code ? 'IconBrand' : 'Icon';
-    const IconComponents = Icons[`${nameConstructor}${name}` as keyof typeof Icons];
+    const IconComponents = Icons[`${nameConstructor}${name}` as keyof typeof Icons] as IconComponent;
     const propriety = clear ? {} : { size, color };
     return <IconComponents {...propriety} /> || <span></span>;
   };
