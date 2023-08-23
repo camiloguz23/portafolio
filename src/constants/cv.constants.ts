12f0ec781a7f83f5bcf7b490656db18b6d14647d
Jeyson Guzman <61909339+camiloguz23@users.noreply.github.com>
2023-08-22 22:27:11 -0500

    Update cv.constants.ts

--- src/constants/cv.constants.ts
+++ src/constants/cv.constants.ts
@@ -2,5 +2,5 @@ import { type CvType } from '@/types';
 
 export const CV: CvType = {
   en: '/documents/JeysonCamilo.pdf',
-  es: '/documents/jeysonCamilo_es.pdf'
+  es: '/documents/JeysonCamilo_es.pdf'
 };
